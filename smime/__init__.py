ad6b8e15545a824e85899c7a8979de2f3871c9f7
G. B. Versiani <guibv@yahoo.com>
2016-06-28 08:34:51 -0300

    * Exporting the encrypt function.

--- smime/__init__.py
+++ smime/__init__.py
@@ -1,3 +1,9 @@
 """Python S/MIME handler."""
 
+__author__ = 'G. B. Versiani'
+__license__ = 'Apache License (2.0)'
 __version__ = '0.0.1'
+
+from .encrypt import encrypt
+
+__all__ = [encrypt, __author__, __license__, __version__]
