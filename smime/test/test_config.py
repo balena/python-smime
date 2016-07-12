7e13b029d0ac77f312c2a4d10153a9c050deb309
G. B. Versiani <guibv@yahoo.com>
2016-07-12 15:45:29 -0300

    Fixed the testdata dir after removing the certificate-transparency dependency.

--- smime/test/test_config.py
+++ smime/test/test_config.py
@@ -2,7 +2,7 @@
 
 import os
 
-CRYPTO_TEST_DATA_DIR = "smime/crypto/testdata/"
+CRYPTO_TEST_DATA_DIR = "smime/testdata/"
 
 def get_test_file_path(filename):
     return os.path.join(os.curdir, CRYPTO_TEST_DATA_DIR, filename)
