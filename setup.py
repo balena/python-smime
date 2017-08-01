c5fd636aa8970d39adf574d22a402c79f8a39d95
Artur Mackowiak <artur.mackowiak@allegrogroup.com>
2017-08-01 08:46:17 +0200

    Remove setup_req in setup

--- setup.py
+++ setup.py
@@ -35,6 +35,5 @@ setup(
     packages=find_packages(exclude=['smime/test', 'smime/crypto/testdata',
         'smime/crypto/tools', '*_test.py']),
     platforms=["all"],
-    setup_requires=['asn1crypto', 'pycrypto'],
     install_requires=['pycrypto', 'asn1crypto'],
 )
